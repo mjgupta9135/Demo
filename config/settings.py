bb6f704f11b2972c0f6d7e2f1965085688a2d459
Mrityunjay <mjguptacse@gmail.com>
2025-06-23 12:17:05 +0530

    Half Completed

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'emp',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'first',
+
+        'USER': 'mjgupta',
+
+        'PASSWORD': 'mjgupta9135',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
@@ -126,3 +135,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Files configuration
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
--- config/views.py
+++ config/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from emp.models import Employee
+
 
 def home(request):
-    return render(request,'home.html')
+    emp=Employee.objects.all()
+    context={
+        'employees':emp
+    }
+    return render(request,'home.html',context)
